4ad1fe0ffbc1843ef86769fa521c2392eb8320a8
Aythami Cordero Soto <aythami.cordero@bitboxonline.com>
2020-01-23 10:48:52 +0000

    Add path

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftMessages",
-            dependencies: [])
+            dependencies: [],
+            path: "Sources")
     ]
 )
